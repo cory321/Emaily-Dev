b61b56eb0fc3a9cc140acc4cd89f7f4fd2f48fa4
cory321 <cory321@gmail.com>
2018-11-11 13:36:34 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const	app = express();
+
+app.get("/", function(req,res){
+	res.send({ hi: "samantha"});
+});
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT);
